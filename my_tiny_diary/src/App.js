ba784dc207579a774269bbddb52d356e5bfcedcc
Sabri Zennoud <sabri.zennoud@epita.fr>
2019-06-30 18:35:05 +0200

    updated style

--- my_tiny_diary/src/App.js
+++ my_tiny_diary/src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
         Firebase.initializeApp(DB_CONFIG);
 
         this.state = {
-            developers: []
+            memory: []
         }
     }
 
@@ -47,19 +47,19 @@ class App extends React.Component {
         let uid = this.refs.uid.value;
 
         if (uid && name && role) {
-            const { developers } = this.state;
-            const devIndex = developers.findIndex(data => {
+            const { memory } = this.state;
+            const devIndex = memory.findIndex(data => {
                 return data.uid === uid
             });
-            developers[devIndex].name = name;
-            developers[devIndex].role = role;
-            this.setState({ developers });
+            memory[devIndex].name = name;
+            memory[devIndex].role = role;
+            this.setState({ memory });
         }
         else if (name && role) {
             const uid = new Date().getTime().toString();
-            const { developers } = this.state;
-            developers.push({ uid, name, role })
-            this.setState({ developers });
+            const { memory } = this.state;
+            memory.push({ uid, name, role })
+            this.setState({ memory });
         }
 
         this.refs.name.value = '';
@@ -67,49 +67,27 @@ class App extends React.Component {
         this.refs.uid.value = '';
     };
 
-    removeData = (developer) => {
-        const { developers } = this.state;
-        const newState = developers.filter(data => {
-            return data.uid !== developer.uid;
+    removeData = (arg) => {
+        const { memory } = this.state;
+        const newState = memory.filter(data => {
+            return data.uid !== arg.uid;
         });
-        this.setState({ developers: newState });
+        this.setState({ memory: newState });
     };
 
-    updateData = (developer) => {
-        this.refs.uid.value = developer.uid;
-        this.refs.name.value = developer.name;
-        this.refs.role.value = developer.role;
+    updateData = (arg) => {
+        this.refs.uid.value = arg.uid;
+        this.refs.name.value = arg.name;
+        this.refs.role.value = arg.role;
     };
 
     render = () => {
-        const { developers } = this.state;
+        const { memory } = this.state;
         return (
             <div className="container">
-                <div className="row">
-                    <div className='col-xl-12'>
-                        <h1>Firebase Development Team</h1>
-                    </div>
-                </div>
                 <div className='row'>
                     <div className='col-xl-12'>
-                        {
-                            developers
-                                .map(developer =>
-                                    <div key={developer.uid} className="card float-left" style={{ width: '18rem', marginRight: '1rem' }}>
-                                        <div className="card-body">
-                                            <h5 className="card-title">{developer.name}</h5>
-                                            <p className="card-text">{developer.role}</p>
-                                            <button onClick={() => this.removeData(developer)} className="btn btn-link">Delete</button>
-                                            <button onClick={() => this.updateData(developer)} className="btn btn-link">Edit</button>
-                                        </div>
-                                    </div>
-                                )
-                        }
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-xl-12'>
-                        <h1>Add new team member here</h1>
+                        <h1>Add Memory Here</h1>
                         <form onSubmit={this.handleSubmit}>
                             <div className="form-row">
                                 <input type='hidden' ref='uid' />
@@ -118,7 +96,7 @@ class App extends React.Component {
                                     <input type="text" ref='name' className="form-control" placeholder="Name" />
                                 </div>
                                 <div className="form-group col-md-6">
-                                    <label>Role</label>
+                                    <label>Description</label>
                                     <input type="text" ref='role' className="form-control" placeholder="Role" />
                                 </div>
                             </div>
@@ -126,12 +104,32 @@ class App extends React.Component {
                         </form>
                     </div>
                 </div>
+                <div className="row">
+                    <div className='col-xl-12'>
+                        <h1>Stored Memory</h1>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-12'>
+                        {
+                            memory
+                                .map(x =>
+                                    <div key={x.uid} className="card float-left" style={{ width: '18rem', marginRight: '1rem' }}>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{x.name}</h5>
+                                            <p className="card-text">{x.role}</p>
+                                            <button onClick={() => this.removeData(x)} className="btn btn-link">Delete</button>
+                                            <button onClick={() => this.updateData(x)} className="btn btn-link">Edit</button>
+                                        </div>
+                                    </div>
+                                )
+                        }
+                    </div>
+                </div>
             </div>
         )
     }
 
-
 }
-
 
 export default App;
